5c573aef5906e7dda3e19ac8907f55195203c134
Jason Hong <JaySunHong@dhcp-v112-249.mobile.uci.edu>
2018-06-09 12:16:49 -0700

    jun 9

--- assets/javascript/jobs.js
+++ assets/javascript/jobs.js
@@ -1,28 +1,7 @@
-$("#searchButton").on("click", function() {
-
+$("#searchButton").on("click", function() { 
     var jobTitle = $("#jobTitle").val().trim();
     var location = $("#location").val().trim();
-    var stateOptions = $("#stateOptions").val();
-    var queryURL = 'https://jobs.github.com/positions.json?description=' + jobTitle + '&location=' + location;
-
-    $.ajax({
-        url: queryURL,
-        method: 'GET',
-        dataType: 'jsonp'
-    }).then(function(response) {
-
-        for (var i = 0; i < response.length; i++) {
-
-            var newP = $("<div class='listing'>");
-
-            newP.append('<img class="companyLogo" src=' + response[i].company_logo + '>');
-            newP.append('<br>' + '<strong>' + response[i].title + '</strong>');
-            newP.append('<br>' + response[i].location);
-            newP.append('<br>' +response[i].company);
-
-            $("#temporary").append(newP);
-            
-        }
-    });
-
+    // save value to localStorage using setItem(key, value)
+    localStorage.setItem('job-title', jobTitle);
+    localStorage.setItem('location', location);
 });
